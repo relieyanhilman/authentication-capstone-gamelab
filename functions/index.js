d2f7e0562262f2d9d8078c5de62307cbc330179b
relieyanhilman <relieyanhilman93@gmail.com>
2022-06-12 23:18:19 +0700

    fixand update endpoint

--- functions/index.js
+++ functions/index.js
@@ -24,64 +24,70 @@ import validateFirebaseIdToken from "./authMiddleware.js";
 app.use(cors({ origin: true }));
 
 app.get("/", async (req, res) => {
-  const snapshot = await db.collection("users").get();
+  try {
+    const snapshot = await db.collection("users").get();
 
-  let users = [];
-  snapshot.forEach((doc) => {
-    let id = doc.id;
-    let data = doc.data();
+    let users = [];
+    snapshot.forEach((doc) => {
+      let id = doc.id;
+      let data = doc.data();
 
-    users.push({ id, ...data });
-  });
-  res.status(200).send(JSON.stringify(users));
+      users.push({ id, ...data });
+    });
+    res.status(200).send(JSON.stringify(users));
+  } catch (err) {
+    res.send({ message: err });
+  }
 });
 
-app.get("/:id", validateFirebaseIdToken, async (req, res) => {
-  const snapshot = await db.collection("users").doc(req.params.id).get();
+//query parameter :id adalah email
+app.get("/:id", async (req, res) => {
+  try {
+    const snapshot = await db.collection("users").doc(req.params.id).get();
 
-  const userId = snapshot.id;
-  const userData = snapshot.data();
-
-  res.status(200).send(JSON.stringify({ id: userId, ...userData }));
-});
-
-app.post("/", validateFirebaseIdToken, async (req, res) => {
-  const user = req.body;
-  await db.collection("users").add(user);
-  res.status(201).send();
-});
-
-app.put("/:id", validateFirebaseIdToken, async (req, res) => {
-  const body = req.body;
-
-  await db.collection("users").doc(req.params.id).update(body);
-
-  res.status(200).send();
-});
-
-app.delete("/:id", validateFirebaseIdToken, async (req, res) => {
-  await db.collection("users").doc(req.params.id).delete();
-
-  res.status(200).send();
-});
-
-app.post("/signup", async (req, res) => {
-  const { email, password, returnSecureToken } = req.body;
-  fetch(
-    "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCC1-wFAEcY_AFH-s8LPuTCBtVC1TqFHmg",
-    {
-      method: "POST",
-      body: JSON.stringify({
-        email: email,
-        password: password,
-        returnSecureToken: true,
-      }),
-      headers: { "Content-Type": "application/json" },
+    const userId = snapshot.id;
+    const userData = snapshot.data();
+    if (!userData) {
+      throw Error("User not found");
     }
-  )
-    .then((res) => res.json())
-    .then((json) => console.log(json))
-    .catch((err) => console.log(err));
+    res.status(200).send(JSON.stringify({ id: userId, ...userData }));
+  } catch (error) {
+    res.send({ message: error.message });
+  }
+});
+
+app.post("/", async (req, res) => {
+  try {
+    const { email, username, password } = req.body;
+    await db.collection("users").doc(email).set({ email, username, password });
+    res.status(201).send("user created");
+  } catch (err) {
+    res.status(400).send({ message: err });
+  }
+});
+
+//query parameter :id adalah email
+app.put("/:id", async (req, res) => {
+  try {
+    const body = req.body;
+
+    await db.collection("users").doc(req.params.id).update(body);
+
+    res.status(200).send("user updated");
+  } catch (err) {
+    res.status(400).send({ message: err });
+  }
+});
+
+//query parameter :id adalah email
+app.delete("/:id", async (req, res) => {
+  try {
+    await db.collection("users").doc(req.params.id).delete();
+
+    res.status(200).send("user deleted");
+  } catch (err) {
+    res.send({ message: err });
+  }
 });
 
 export const user = functions.region("asia-southeast2").https.onRequest(app);
